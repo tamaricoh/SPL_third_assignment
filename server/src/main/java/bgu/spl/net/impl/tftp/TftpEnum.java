b12d5a4aa954ee0bfd0fc975c8b034a21a62e145
tamaricoh <tamarcmail@gmail.com>
2024-03-10 18:49:44 +0200

    TftpProtocol - start implementation process (RRQ, WRQ, DATA)

---
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpEnum.java
@@ -0,0 +1,90 @@
+package bgu.spl.net.impl.tftp;
+
+enum TFTPRequest{
+    NONE,
+    RRQ,
+    WRQ,
+    DATA,
+    ACK,
+    ERROR,
+    DIRQ,
+    LOGRQ,
+    DELRQ,
+    BCAST,
+    DISC
+}
+
+public class TftpEnum {
+
+    public static TFTPRequest create(){
+        return TFTPRequest.NONE;
+    }
+
+    // public static int decodeOperationEnum(TFTPRequest opCode) {
+    //     switch (opCode) {
+    //         case RRQ:
+    //             return 1;
+    //         case WRQ:
+    //             return 2;
+    //         case DATA:
+    //             return 3;
+    //         case ACK:
+    //             return 4;
+    //         case ERROR:
+    //             return 5;
+    //         case DIRQ:
+    //             return 6;
+    //         case LOGRQ:
+    //             return 7;
+    //         case DELRQ:
+    //             return 8;
+    //         case BCAST:
+    //             return 9;
+    //         case DISC:
+    //             return 10;
+    //         default:
+    //             return -1;
+    //     }
+    // }
+    
+    public static TFTPRequest decodeOperationCode(short opCode) {
+        switch (opCode) {
+            case 1:
+                return TFTPRequest.RRQ;
+            case 2:
+                return TFTPRequest.WRQ;
+            case 3:
+                return TFTPRequest.DATA;
+            case 4:
+                return TFTPRequest.ACK;
+            case 5:
+                return TFTPRequest.ERROR;
+            case 6:
+                return TFTPRequest.DIRQ;
+            case 7:
+                return TFTPRequest.LOGRQ;
+            case 8:
+                return TFTPRequest.DELRQ;
+            case 9:
+                return TFTPRequest.BCAST;
+            case 10:
+                return TFTPRequest.DISC;
+            default:
+                return null;
+        }
+    }
+
+    public static Boolean TFTPRequestEndsWithZero(TFTPRequest opEnum){
+        switch (opEnum) {
+            case LOGRQ:
+            case DELRQ:
+            case RRQ:
+            case WRQ:
+            case BCAST:
+            case ERROR:
+                return true;
+            default:
+            return false;
+        }
+    }
+}
---
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpFileInputStream.java
@@ -0,0 +1,17 @@
+package bgu.spl.net.impl.tftp;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class TftpFileInputStream {
+
+    private FileInputStream filePath;
+
+    public TftpFileInputStream(String filePath) throws FileNotFoundException{
+        try {
+            this.filePath = new FileInputStream(filePath);
+        } catch (FileNotFoundException e) {
+            throw e;
+        }
+    }
+    
+}
