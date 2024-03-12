d39b3bbcae2b003aa4a0a3550e01eb1f76966e3e
Yarden-Greenpeter <ygreenpeter@gamil.com>
2024-03-12 19:16:48 +0200

    implemented the packet generator

---
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpPacketGenerator.java
@@ -0,0 +1,64 @@
+package bgu.spl.net.impl.tftp;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+
+
+public class TftpPacketGenerator{
+
+    Charset encode = StandardCharsets.UTF_8;
+    public TftpPacketGenerator(){}
+    
+
+    private byte[] generateACk(byte blockNum){ 
+        byte[] ACKpacket = new byte[4];
+        ACKpacket[0] = 0;
+        ACKpacket[1] = 4;
+        ACKpacket[2] = 0;
+        ACKpacket[3] = blockNum;
+        return ACKpacket;
+    }
+
+    private byte[] generateError(byte errorNum, String ErrorStr){       // Data, BCAST
+        byte[] msg = ErrorStr.getBytes(encode);
+        byte[] ERRORpacket = new byte[msg.length + 5];
+        ERRORpacket[0] = 0; ERRORpacket[1] = 5; ERRORpacket[2] = 0; ERRORpacket[3] = errorNum;
+        fill(ERRORpacket, msg, 4, ERRORpacket.length-2); ERRORpacket[ERRORpacket.length-1] = 0;
+        return ERRORpacket;
+        
+    }
+
+
+    /**
+     * 
+     * @param added - is equel to1 if true 0 otherwish
+     * 
+     * 
+     * 
+     */
+    private byte[] generateBCAST(byte added, String fileName){ 
+        byte[] msg = fileName.getBytes(encode);
+        byte[] BCASTpacket = new byte[msg.length + 4];
+        BCASTpacket[0] = 0; BCASTpacket[1] = 9; BCASTpacket[2] = added;
+        fill(BCASTpacket, msg, 3, BCASTpacket.length-2); BCASTpacket[BCASTpacket.length-1] = 0;
+        return BCASTpacket;
+        
+    }
+
+    private byte[] generateDATA(byte[] size,byte[] blockNum,  byte[] data){
+        byte[] DATApacket = new byte[data.length + 6];
+        DATApacket[0] = 0; DATApacket[1] = 3; DATApacket[2] = size[0]; DATApacket[3] = size[1];
+        DATApacket[4] = blockNum[0]; DATApacket[5] = blockNum[1];
+        fill(DATApacket, data, 6, DATApacket.length-1);
+        return DATApacket;
+        
+    }
+
+    private void fill(byte[] a, byte[] b, int start, int end){
+        for(int i = 0; i<b.length & i+start <= end; i++){
+            a[start+i] = b[i];
+        }
+    }
+
+
+}
