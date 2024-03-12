c616a5f91874c67ca35da681bb0b2491897f016a
Yarden-Greenpeter <ygreenpeter@gamil.com>
2024-03-12 19:53:24 +0200

    improves on generating packates

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpPacketGenerator.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpPacketGenerator.java
@@ -10,36 +10,38 @@ public class TftpPacketGenerator{
     public TftpPacketGenerator(){}
     
 
-    private byte[] generateACk(byte[] blockNum){ 
+    public byte[] generateACk(byte[] blockNum){ 
         byte[] ACKpacket = new byte[4];
-        ACKpacket[0] = 0;
-        ACKpacket[1] = 4;
-        fill()
+        ACKpacket[0] = 0; ACKpacket[1] = 4;
+        fill(ACKpacket, blockNum,2,3);
         return ACKpacket;
     }
 
-    private byte[] generateError(byte[] errorNum, byte[] Errormsg){       // Data, BCAST
+    public byte[] generateError(byte[] errorNum, byte[] Errormsg){       // Data, BCAST
         byte[] ERRORpacket = new byte[Errormsg.length + 5];
-        ERRORpacket[0] = 0; ERRORpacket[1] = 5; ERRORpacket[2] = errorNum[0]; ERRORpacket[3] = errorNum[1];
-        fill(ERRORpacket, Errormsg, 4, ERRORpacket.length-2); ERRORpacket[ERRORpacket.length-1] = 0;
+        ERRORpacket[0] = 0; ERRORpacket[1] = 5;
+        fill(ERRORpacket, errorNum,2,3);
+        fill(ERRORpacket, Errormsg, 4, ERRORpacket.length-2);
+         ERRORpacket[ERRORpacket.length-1] = 0;
         return ERRORpacket;
         
     }
 
 
     /**
-     * @param added - is equel to1 if true 0 otherwish 
+     * @param added - is equel to 1 if true, or 0 otherwish 
      */
-    private byte[] generateBCAST(byte[] added, byte[] fileName){ 
-        byte[] msg = fileName.getBytes(encode);
-        byte[] BCASTpacket = new byte[msg.length + 4];
-        BCASTpacket[0] = 0; BCASTpacket[1] = 9; BCASTpacket[2] = added;
-        fill(BCASTpacket, msg, 3, BCASTpacket.length-2); BCASTpacket[BCASTpacket.length-1] = 0;
+    public byte[] generateBCAST(byte[] added, byte[] fileName){ 
+        byte[] BCASTpacket = new byte[fileName.length + 4];
+        BCASTpacket[0] = 0; BCASTpacket[1] = 9;
+        BCASTpacket[2] = added[0];
+        fill(BCASTpacket, fileName, 3, BCASTpacket.length-2);
+         BCASTpacket[BCASTpacket.length-1] = 0;
         return BCASTpacket;
         
     }
 
-    private byte[] generateDATA(byte[] size,byte[] blockNum,  byte[] data){
+    public byte[] generateDATA(byte[] size,byte[] blockNum,  byte[] data){
         byte[] DATApacket = new byte[data.length + 6];
         DATApacket[0] = 0; DATApacket[1] = 3; DATApacket[2] = size[0]; DATApacket[3] = size[1];
         DATApacket[4] = blockNum[0]; DATApacket[5] = blockNum[1];
